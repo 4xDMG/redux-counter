c4469af10620efb116597cff50ff9b3c084e86a6
4xDMG <dajtait87@gmail.com>
2017-07-30 14:32:53 +1000

    Finsihed redux-counter.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.incrementAsync = this.incrementAsync.bind(this);
+    this.incrementIfOdd = this.incrementIfOdd.bind(this);
+  }
+
+  incrementAsync() {
+    setTimeout(this.props.onIncrement, 1000);
+  }
+
+  incrementIfOdd() {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement();
+    }
+  }
+
   render() {
+    const { value, onIncrement, onDecrement } = this.props;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>+</button>
+        {' '}
+        <button onClick={onDecrement}>-</button>
+        {' '}
+        <button onClick={this.incrementAsync}>Async +</button>
+        {' '}
+        <button onClick={this.incrementIfOdd}>+ if odd</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import counter from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counter);
+
+const render = () => ReactDOM.render(
+  <App
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  document.getElementById('root')
+);
+
+render();
+store.subscribe(render);
